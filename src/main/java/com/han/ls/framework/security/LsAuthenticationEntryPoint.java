ee5d90dba917cc4735b3c8955d85911d9b7fada5
han <2314540340@qq.com>
2022-06-11 11:55:35 +0800

    .

--- src/main/java/com/han/ls/framework/security/LsAuthenticationEntryPoint.java
+++ src/main/java/com/han/ls/framework/security/LsAuthenticationEntryPoint.java
@@ -1,7 +1,7 @@
 package com.han.ls.framework.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.han.ls.common.enums.ResultStatus;
+import com.han.ls.common.utils.JsonUtils;
 import com.han.ls.framework.web.domain.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
@@ -22,7 +22,6 @@ public class LsAuthenticationEntryPoint implements AuthenticationEntryPoint {
         log.error("未登录访问api，请求地址{}", request.getRequestURI());
         response.setCharacterEncoding("utf-8");
         response.setContentType("application/json;charset=utf-8");
-        ObjectMapper objectMapper = new ObjectMapper();
-        response.getWriter().write(objectMapper.writeValueAsString(R.error(ResultStatus.INVALID_TOKEN)));
+        response.getWriter().write(JsonUtils.toJson(R.error(ResultStatus.INVALID_TOKEN)));
     }
 }
