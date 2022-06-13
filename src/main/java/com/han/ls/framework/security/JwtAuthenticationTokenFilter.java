2b231f7a5931299e86792d61f318d4b06a81e881
han <2314540340@qq.com>
2022-06-13 14:05:47 +0800

    .

--- src/main/java/com/han/ls/framework/security/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/han/ls/framework/security/JwtAuthenticationTokenFilter.java
@@ -43,6 +43,7 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
             }
             if (SecurityContextHolder.getContext().getAuthentication() == null) {
                 UserDetails userDetails = userDetailsService.loadUserByUsername(openId);
+                // TODO 是否要在这里验证用户账户是否可用
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
